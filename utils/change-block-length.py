e9a628f984ff7cf3ade38fc54445092a503e9099
Julian Andres Klode <klode@mathematik.uni-marburg.de>
2014-09-04 16:45:04 +0200

    utils/change-block-length.py: Read code changes from git and adjust references automatically

--- utils/change-block-length.py
+++ utils/change-block-length.py
@@ -16,48 +16,109 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import re
 import sys
-
-FILE = sys.argv[1]
-FIRST = re.compile(r"\\lstinputlisting\[(.*)firstline=([0-9]+)(.*)\]{%s}" %  FILE)
-LAST = re.compile(r"\\lstinputlisting\[(.*)lastline=([0-9]+)(.*)\]{%s}" % FILE)
-try:
-    START = int(sys.argv[2])
-    END   = (int(sys.argv[3]), int(sys.argv[4]))
-    DIFF = (END[1] - END[0])
-except:
-    print("Usage: %s <SOURCE FILE> <START OF BLOCK> <OLD END OF BLOCK> <NEW END> <FILES ...>", file=sys.stderr)
-    sys.exit(1)
-
-def sub_first(match):
-    if int(match.group(2)) > END[0]:
-        return "\\lstinputlisting[%sfirstline=%s%s]{%s}" % (match.group(1),
-                                                            int(match.group(2)) + DIFF,
-                                                            match.group(3),
-                                                            FILE)
-
-    return match.group(0)
-
-def sub_last(match):
-    if int(match.group(2)) > END[0]:
-        return "\\lstinputlisting[%slastline=%s%s]{%s}" % (match.group(1),
-                                                            int(match.group(2)) + DIFF,
-                                                            match.group(3),
-                                                            FILE)
-    return match.group(0)
+import subprocess
+import glob
 
 
-def subst(lines):
-    for line in lines:
-        oline = line
-        line = FIRST.sub(sub_first, line)
-        line = LAST.sub(sub_last, line)
-        yield line
+TEXFILES = glob.glob("*.tex") + glob.glob("*/*.tex")
+REGEX = r"\\lstinputlisting\[%s%s=%s%s\]{%s}"
+QUERY = "\\lstinputlisting[%s%s=%s%s]{%s}"
+DIFFREGEX = r"^\+\+\+ b/(.*)|^@@ -([0-9]+),([0-9]+) \+([0-9]+),([0-9]+) @@"
+DIFFRE = re.compile(DIFFREGEX, re.M)
 
-for file in sys.argv[5:]:
-    with open(file) as lines:
-        newlines = list(subst(lines))
-    with open(file, "w") as target:
-        for line in newlines:
-            target.write(line)
+
+def apply_hunk(fname, start, end_old, end_new):
+    """Apply a change in block length to the LaTeX files."""
+    diff = (end_new - end_old)
+    first = REGEX % ("(.*)", "firstline", "([0-9]+)", "(.*)", fname)
+    last = REGEX % ("(.*)", "lastline", "([0-9]+)", "(.*)", fname)
+
+    def sub_first(match):
+        if int(match.group(2)) > end_old:
+            return QUERY % (match.group(1), "firstline",
+                            int(match.group(2)) + diff, match.group(3), fname)
+
+        return match.group(0)
+
+    def sub_last(match):
+        if int(match.group(2)) > end_old:
+            return QUERY % (match.group(1), "lastline",
+                            int(match.group(2)) + diff, match.group(3), fname)
+
+        return match.group(0)
+
+    def subst(lines):
+        for line in lines:
+            oline = line
+            line = re.sub(first, sub_first, line)
+            line = re.sub(last, sub_last, line)
+            yield line
+
+    for file in TEXFILES:
+        with open(file) as lines:
+            newlines = list(subst(lines))
+        with open(file, "w") as target:
+            for line in newlines:
+                target.write(line)
+
+
+def main_git():
+    """Determines and applies block length changes using git."""
+    proc = subprocess.Popen(["git", "diff", "scala"], stdout=subprocess.PIPE)
+    out = proc.communicate()[0].decode()
+
+    RE = re.compile("([-+])Subproject commit (.*)")
+    frm = None
+    to = None
+    for mode, commit in RE.findall(out):
+        if mode == "-":
+            frm = commit
+        elif mode == "+":
+            to = commit
+        else:
+            raise ValueError("Unkown mode: %s" % mode)
+    if frm is None or to is None:
+        raise ValueError("Cannot calculate diff from %s to %s" % (frm, to))
+
+    if "dirty" in to:
+        to = []
+    else:
+        to = [to]
+    proc = subprocess.Popen(["git", "-C", "scala", "diff", frm] + to,
+                            stdout=subprocess.PIPE)
+    diff = proc.communicate()[0].decode()
+    fname = None
+
+    for path, start1, lines1, start2, lines2 in DIFFRE.findall(diff):
+        if path:
+            fname = path
+        else:
+            # We already applied previous changes, so the new start value
+            # (that is, start2) is the one we are interested in.
+            apply_hunk("scala/" + fname, int(start2),
+                       int(start2) + int(lines1),
+                       int(start2) + int(lines2))
+
+
+def main(argv):
+    """If no arguments are given, this determines the changes using git."""
+    if len(argv) == 1:
+        return main_git()
+
+    try:
+        start = int(argv[2])
+        end_old = int(argv[3])
+        end_new = int(argv[4])
+    except Exception as e:
+        print("Usage: %s <SOURCE fname> <START OF BLOCK> " +
+              "<OLD END OF BLOCK> <NEW END>", file=sys.stderr)
+        sys.exit(1)
+
+    apply_hunk(argv[1], start, end_old, end_new)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
